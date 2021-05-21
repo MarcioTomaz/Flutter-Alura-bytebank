98f94bf017911ad9e46277801450590795520883
Marcio Tomaz <marciotmaz@gmail.com>
2021-05-21 11:33:03 -0300

    criando layout

--- lib/main.dart
+++ lib/main.dart
@@ -22,9 +22,38 @@ class FormularioTransferencia extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Criando transfêrencia"),),
-      body: Text("aaaaaa"),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                labelText: 'Número da conta',
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),   Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                icon: Icon( Icons.monetization_on),
+                labelText: 'Valor',
+                hintText: '0.00',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(onPressed: () {  }, child: Text("Confirmar"),),
+        ],
+      ),
     );
-
   }
 }
 
